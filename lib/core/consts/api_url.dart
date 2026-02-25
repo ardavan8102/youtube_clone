25fe9cf775c66a7656b9a4a793060dea8cf2f729
ardavan8102 <eskandariardavan81@gmail.com>
2026-02-25 17:29:08 +0330

    chore: ignore api_url.dart

--- lib/core/consts/api_url.dart
+++
@@ -1,8 +0,0 @@
-class YoutubeApiUrls {
-
-  static const String apiKey = 'AIzaSyAjQA0hbPkcQTs-4fzNNwb7nx9rVC61cAM';
-  static const String youtubeApiBaseURL = 'https://www.googleapis.com/youtube/v3/';
-
-  static const String youtubeSearchApi = '${youtubeApiBaseURL}search';
-
-}
